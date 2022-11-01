e10595f2138103a49fe175a083617b81eac31fa9
root <root@ip-172-31-218-9.ec2.internal>
2022-11-01 19:35:29 +0000

    task3 completed

--- automation.sh
+++ automation.sh
@@ -1,3 +1,4 @@
+
 s3_bucket="upgrad-ashish"
 timestamp=$(date '+%d%m%Y-%H%M%S') 
 myname="ashish"
@@ -22,3 +23,38 @@ tar -cvf ../../tmp/${myname}-httpd-logs-${timestamp}.tar /../../var/log/apache2
 aws s3 \
 cp /tmp/${myname}-httpd-logs-${timestamp}.tar \
 s3://${s3_bucket}/${myname}-httpd-logs-${timestamp}.tar 
+
+inventory=/var/www/html/inventory.html
+log="httpd-logs"
+timestamp=$(stat --printf=%y /tmp/$filename | cut -d.  -f1)
+file=${filename##*.}
+size=$(ls -lh /tmp/${filename} | cut -d " " -f5)
+
+echo "File-Name : $filename"
+echo "Log_Type : $log"
+echo "Time_Of_Creation : $timestamp"
+echo "Type_Of_File : $file"
+echo "File_Size : $size"
+
+
+if  test -f "$inventory"
+then
+	echo "<br>${log}&nbsp;&nbsp;&nbsp;&nbsp;${timestamp}&nbsp;&nbsp;&nbsp;&nbsp;${file}&nbsp;&nbsp;&nbsp;&nbsp;${size}">>"${inventory}"
+	echo "Inventory file updated"
+else
+        echo "Making '$inventory'"
+        `touch ${inventory}`
+        echo "<b>Log Type&nbsp;&nbsp;&nbsp;&nbsp;Date Created&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Type&nbsp;&nbsp;&nbsp;Size</b>">>"${inventory}"
+        echo "<br>${log}&nbsp;&nbsp;&nbsp;&nbsp;${timestamp}&nbsp;&nbsp;&nbsp;&nbsp;${file}&nbsp;&nbsp;&nbsp;&nbsp;${size}">>"${inventory}"
+        echo "UPDATED '$inventory' "
+fi
+
+
+
+
+cron_file="/etc/cron.d/automation"
+if [ ! -f "$cron_file" ]
+then
+touch "$cron_file"
+echo "00 00 * * * root /root/Automation_Project/automation.sh" > "$cron_file"
+fi
